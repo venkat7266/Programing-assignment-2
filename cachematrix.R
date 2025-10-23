56bb446a57b6a71dd7de24888370d4764e113015
Naman Garg <naman@Namans-MacBook-Air.local>
2025-10-24 05:20:22 +0530

    defined the functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function() {
+    i <<- solve(x)
+  }
+  getinverse <- function() i
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
@@ -12,4 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)){
+    message("from the cache")
+    return(i)
+  }
+  i <- x$setinverse()
+  i
 }
