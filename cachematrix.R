ee4787b53e24cd5e92cede3c697679e9895b1041
Naman Garg <naman@Namans-MacBook-Air.local>
2025-10-24 05:54:10 +0530

    added comments to the function

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## this function returns a list of functions.
+## the functions within the list all have their scope set within the parent function
+## the functions within the list allow us to cache a matrix, retrieve a cached matrix, 
+## cache inverse of the cached matrix, retrieve cached inverse of the a cached matrix
 
 makeCacheMatrix <- function(x = matrix()) {
   i <- NULL
@@ -18,7 +21,9 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## this function returns the inverse of the cached matrix within the variable
+## the functions first checks if the inverse is cached and sends the cached results back if condition is true
+## if the inverse of the matrix isn't cached the the function solves for the inverse, caches it, and shares back the result
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
